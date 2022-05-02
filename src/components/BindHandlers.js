48e7c636f74294c82a2f80323a0f6aa7663d6bfc
Promtech <uwagboepromise5@gmail.com>
2022-05-02 00:00:00 -0600

    [New]--Initial commit

---
+++ src/components/BindHandlers.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+ class BindHandlers extends Component {
+     constructor(props) {
+       super(props)
+     
+       this.state = {
+          message: 'Hello'
+       }
+    //    this.clickHandler = this.clickHandler.bind(this)
+     }
+     clickHandler = ()=>{
+         this.setState({
+             message: 'Welcome!'
+         })
+         console.log(this)
+     }
+  render() {
+    return (
+      <div>
+          <div>
+          {this.state.message}
+          </div>
+          {/* <button onClick={this.clickHandler.bind(this)}>Click</button> */}
+          {/* <button onClick={()=> this.clickHandler()}>Click</button> */}
+          <button onClick={this.clickHandler}>Click</button>
+          </div>
+    )
+  }
+}
+
+export default BindHandlers
